c40a94c52674debf8980d064a3123f5ca7bf6d5d
Kristian Polso <kristian@vaiste.com>
2016-08-20 17:43:06 +0300

    Crossdomains

--- instagram.py
+++ instagram.py
@@ -3,7 +3,9 @@
 
 import requests
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, current_app
+from datetime import timedelta
+from functools import update_wrapper
 
 access_token = '1555940494.e029fea.f4f3539e108d4079b960d11199a97a9f'
 client_secret = '715faafee65a4a7eb498c216adc72771'
@@ -11,6 +13,51 @@ client_secret = '715faafee65a4a7eb498c216adc72771'
 baseUrl = "https://api.instagram.com/v1"
 
 app = Flask(__name__)
+
+
+def crossdomains(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 def getNearbyLocationIds(lat, lng):
     r = requests.get(baseUrl + "/locations/search?lat=" + lat + "&lng=" + lng + "&access_token=" + access_token)
@@ -41,8 +88,8 @@ def getNearbyRecentMediaByLatLon(lat, lng):
             print 'data'
             if d['type'] == 'video':
                 info = {'thumbnail': d['images']['thumbnail']['url'],
-                        'location': {'lat': lat,
-                                     'lng': lng},
+                        'location': {'lat': d['location']['latitude'],
+                                     'lng': d['location']['longitude']},
                         'videoUrl': d['videos']['low_resolution']['url'],
                         'url': d['link']
                 }
@@ -65,14 +112,13 @@ def inGet():
 #    return 'u searched ' + lat
 
     
-@app.route('/get/location/search', methods=['GET'])
+@app.route('/get/location/search', methods=['GET', 'OPTIONS'])
+@crossdomains(origin='*')
 def getIds():    
     lat = request.args.get('lat')
     lon = request.args.get('lon')
     ids = getNearbyRecentMediaByLatLon(lat, lon)
     response = jsonify(data = ids)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', '*')
     return response
 
 

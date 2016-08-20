a532c0fbc121e865e75cb0c4dd8e4a696ca0006f
Omar El-Begawy <omar.elbegawy@gmail.com>
2016-08-20 16:39:55 +0300

    backend commit

---
+++ instagram.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#from instagram.client import InstagramAPI
+
+import requests
+import json
+from flask import Flask, request, jsonify
+
+access_token = '1555940494.e029fea.f4f3539e108d4079b960d11199a97a9f'
+client_secret = '715faafee65a4a7eb498c216adc72771'
+
+baseUrl = "https://api.instagram.com/v1"
+
+app = Flask(__name__)
+
+
+def getNearbyLocationIds(lat, lng):
+    r = requests.get(baseUrl + "/locations/search?lat=" + lat + "&lng=" + lng + "&access_token=" + access_token)
+    if(r.ok):
+        ids = []
+        print r.status_code
+        data = json.loads(r.text)
+        for d in data['data']:
+            if int(d['id']) != 0:
+                print d['id']
+                ids.append(d['id'])
+        return ids
+    
+def getNearbyRecentMediaById(ids):
+    list = []
+
+
+    for i in ids:
+        print i
+
+def getNearbyRecentMediaByLatLon(lat, lng):
+    list = []
+    r = requests.get(baseUrl + "/media/search?lat=" + lat + "&lng=" + lng + "&access_token=" + access_token)
+    if(r.ok):
+        ids = []
+        data = json.loads(r.text)
+        for d in data['data']:
+            print 'data'
+            if d['type'] == 'video':
+                info = {'thumbnail': d['images']['thumbnail']['url'],
+                        'location': {'lat': lat,
+                                     'lng': lng},
+                        'videoUrl': d['videos']['low_resolution']['url'],
+                        'url': d['link']
+                }
+                ids.append(info)
+        return ids
+                     
+        
+    
+@app.route('/', methods=['GET'])
+def helloWorld():
+    return 'hello'
+
+@app.route('/get/location', methods=['GET'])
+def inGet():
+    return 'u in get location'
+
+#@app.route('/get/location/search', methods=['GET'])
+#def inGetLoc():
+#    lat = request.args.get('lat')
+#    return 'u searched ' + lat
+
+    
+@app.route('/get/location/search', methods=['GET'])
+def getIds():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    ids = getNearbyRecentMediaByLatLon(lat, lon)
+    return jsonify(data = ids)
+
+
+#    ids = getNearbyLocationIds(lat, lon)
+#    idList = []
+#    print ids
+#    for id in ids:
+#        id = {'id': id}
+#        idList.append(id)
+#        print id
+#    return jsonify(data = idList )
+
+
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+#        app.run(debug=True, host='0.0.0.0')
+    
+
+
+
+lat = "60.4518"
+lon = "22.2666"
+
+lat = "48.858844"
+lon =  "2.294351"
+
+lat = "48"
+lon = "2"
+
+ids = getNearbyLocationIds(lat, lon)
+print ids
+
+r = requests.get(baseUrl + "/locations/search?lat=48&lng=2&distance=500&access_token=" + access_token)
+
+
+#r = requests.get(baseUrl + "/nofilter/media/recent?access_token=" + access_token)
+
+
+
+#if(r.ok):
+#  print r.status_code
+#  print r.encoding
+#  print r.headers['content-type']
+#  print r.json()
+#  data = r.json
+#  data = json.loads(r.text)
+
+#  print data["data"]
+
+
+#  for d in data['data']:
+#      if d != '0':
+#          print d['id']
+                 
+
+          
